cd1ed8f524c7ff4241bcca3e4a48752cccc9fd32
AlexeyBobkov <alekseixxxbobkov@bk.ru>
2020-10-18 01:11:44 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let out = false;
+
+  let arr = bracketsConfig.map( (e) => e.join("") ); /* прохождение циклом по скобкам */
+
+  for (let i = 0;i < arr.length; i++){ /* прохождение по условию */
+    if(str.includes(arr[i])){
+
+      str = str.replace(arr[i],"");
+      
+      i =   -1;
+
+    }
+  }
+  if(str.length==0){
+    out=true 
+  }
+    return out;
 }
+
+ 
